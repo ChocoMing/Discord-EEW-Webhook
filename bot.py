08cdd15d97f3654dc77800dd56d4276636c21704
ChocoMing <65898245+ChocoMing@users.noreply.github.com>
2021-08-19 09:52:20 +0800

    修改設定方式

--- bot.py
+++ bot.py
@@ -1,10 +1,18 @@
 import re
+from aiohttp.helpers import TOKEN
 import discord
 import datetime
 from discord import Webhook, AsyncWebhookAdapter
 import aiohttp
 
 bot = discord.Client()
+
+#以下你你可以設定的變數
+city = "XX市" #你要顯示的縣、市 例:台北市
+Area = "XX區" #你要顯示的區 例:松山區
+Webhook_URL = "你的Webhook網址"
+BOT_TOKEN = "你的TOKEN" #到https://discord.com/developers/applications 取得(需有Discord帳號)
+#------------------------------
 
 @bot.event
 async def on_ready():
@@ -15,14 +23,14 @@ async def on_ready():
                           timestamp=datetime.datetime.utcnow(),
                           description='慎防搖晃(預估震度)', color=0x03b2f8)
     embed.set_author(name='Taiwan EEW System',
-                     icon_url='icon_url')
-    embed.add_field(name='XX市', value=f'XX區 {ggg}')
+                     icon_url='https://media.discordapp.net/attachments/345147297539162115/732527807435112478/EEW.png')
+    embed.add_field(name=city, value= f"{Area} {ggg}")
 
     async with aiohttp.ClientSession() as session:
-        for webhook_url in 'webhook_url':
+        for webhook_url in [Webhook_URL]:
             webhook = Webhook.from_url(
                 webhook_url, adapter=AsyncWebhookAdapter(session))
-            await webhook.send(embed=embed, username="webhook_name", avatar_url="icon_url")
+            await webhook.send(embed=embed, username="地牛Wake Up!", avatar_url="https://scontent.ftpe4-1.fna.fbcdn.net/v/t1.6435-9/31783240_381366989015031_1910938126604304384_n.png?_nc_cat=101&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=8ND3oeqVlMoAX_bm4Wu&_nc_ht=scontent.ftpe4-1.fna&oh=804e6c88f560d73055ea327509a26b08&oe=61445067")
     await bot.close()
 
-bot.run("TOKEN")
+bot.run(BOT_TOKEN)
